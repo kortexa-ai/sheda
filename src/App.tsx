a71ed96c90738d54115ddef3e4367922541e93f4
Franci Penov <francip@gmail.com>
2025-03-25 17:52:47 -0700

    Macos support 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import type {ReactNode} from 'react';
-import { Text, useColorScheme, View } from 'react-native';
+import { Text, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App(): ReactNode {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle && { flex: 1}}>
-      <Text style={backgroundStyle && {backgroundColor: 'white'}}>Shaderism</Text>
+    <SafeAreaView style={{ backgroundColor: isDarkMode ? 'black' : 'white', flex: 1}}>
+      <Text style={{ color: isDarkMode ? 'white' : 'black', textAlign: 'center'}}>Shaderism</Text>
     </SafeAreaView>
   );
 }
