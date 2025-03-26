e283b6192e5647b64b425211c5a6d1e11fb7f25c
Franci Penov <francip@gmail.com>
2025-03-25 18:51:39 -0700

    Windows

---
+++ windows/shaderism/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::shaderism::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::shaderism::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
